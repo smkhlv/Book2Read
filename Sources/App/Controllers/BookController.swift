feebc9f1de52453a74755129f6d094b7f42ba687
Sergei <mikhaylovserj@gmail.com>
2024-01-29 19:47:39 +0600

    updated book methods with getting files

--- Sources/App/Controllers/BookController.swift
+++ Sources/App/Controllers/BookController.swift
@@ -16,16 +16,17 @@ struct BookController: RouteCollection {
         tokenProtected.post("create", use: create)
         tokenProtected.get("find", use: findBook)
         tokenProtected.get("all", use: getAllBooks)
+        tokenProtected.get(":itemID", use: getDetail)
     }
     
-    func create(req: Request) async throws -> Book {
+    func create(req: Request) async throws -> Book.Information {
         let bookData = try req.content.decode(Book.self)
         try await bookData.save(on: req.db)
-        return bookData
+        return bookData.asInformation()
     }
     
-    func findBook(req: Request) throws -> EventLoopFuture<[Book]> {
-        let searchQuery = try req.query.decode(SearchBookQuery.self)
+    func findBook(req: Request) throws -> EventLoopFuture<[Book.Information]> {
+        let searchQuery = try req.query.decode(Book.SearchBookQuery.self)
         return Book.query(on: req.db)
             .group(.or) { or in
                 if let title = searchQuery.title {
@@ -34,31 +35,31 @@ struct BookController: RouteCollection {
                 if let genre = searchQuery.genre {
                     or.filter(\.$genre == genre)
                 }
-//                if let description = searchQuery.description {
-//                    or.filter(\.$description == description)
-//                }
-//                if let authorName = searchQuery.authorName {
-//                    or.filter(\.$authorName == authorName)
-//                }
+                if let description = searchQuery.description {
+                    or.filter(\.$description == description)
+                }
+                if let authorName = searchQuery.authorName {
+                    or.filter(\.$authorName == authorName)
+                }
             }
             .paginate(for: req)
             .map { page in
-                page.items
+                page.items.map { $0.asInformation() }
             }
     }
     
-    func getAllBooks(req: Request) throws -> EventLoopFuture<[Book]> {
+    func getAllBooks(req: Request) throws -> EventLoopFuture<[Book.Information]> {
         return Book.query(on: req.db)
             .paginate(for: req)
             .map { page in
-                page.items
+                page.items.map { $0.asInformation() }
             }
     }
-}
-
-struct SearchBookQuery: Content {
-    let title: String?
-    let genre: String?
-    let description: String?
-    let authorName: String?
+    
+    func getDetail(req: Request) async throws -> Book {
+        guard let itemID = req.parameters.get("itemID", as: UUID.self) else {
+            throw Abort(.badRequest, reason: "Invalid book ID")
+        }
+        return try await Book.find(itemID, on: req.db).unsafelyUnwrapped
+    }
 }
--- Sources/App/Models/Book.swift
+++ Sources/App/Models/Book.swift
@@ -23,48 +23,103 @@ final class Book: Model, Content {
     @Field(key: "genre")
     var genre: String
     
-//    @Field(key: "price")
-//    var price: Double
-//    
-//    @Field(key: "progress")
-//    var progress: Double?
-//    
-//    @Field(key: "rating")
-//    var rating: Double?
-//    
-//    @Field(key: "publishDate")
-//    var publishDate: Date
-//    
-//    @Field(key: "ratingCount")
-//    var ratingCount: Int
-//    
-//    @Field(key: "description")
-//    var description: String
-//    
-//    @Field(key: "isPaperVersionAvaliable")
-//    var isPaperVersionAvaliable: Bool
-//    
-//    @Field(key: "pageCount")
-//    var pageCount: Int
-//    
-//    @Field(key: "isAudibleAvaliable")
-//    var isAudibleAvaliable: Bool
-//
-//    @Field(key: "backCoverText")
-//    var backCoverText: String
-//    
-//    @Field(key: "coverImageUrl")
-//    var coverImageUrl: String
-//    
-//    @Field(key: "authorName")
-//    var authorName: String
+    @Field(key: "price")
+    var price: Double?
+    
+    @Field(key: "rating")
+    var rating: Double?
+    
+    @Field(key: "publishDate")
+    var publishDate: Date?
+    
+    @Field(key: "ratingCount")
+    var ratingCount: Int?
+    
+    @Field(key: "description")
+    var description: String?
+    
+    @Field(key: "isPaperVersionAvaliable")
+    var isPaperVersionAvaliable: Bool?
+    
+    @Field(key: "pageCount")
+    var pageCount: Int?
+    
+    @Field(key: "isAudibleAvaliable")
+    var isAudibleAvaliable: Bool?
+
+    @Field(key: "backCoverText")
+    var backCoverText: String?
+    
+    @Field(key: "coverImageUrl")
+    var coverImageUrl: String?
+    
+    @Field(key: "authorName")
+    var authorName: String?
+    
+    @Field(key: "bookData")
+    var bookData: Data?
     
     init() { }
 
-    init(id: UUID? = nil, isbn: Int, title: String, genre: String) {
+    init(id: UUID? = nil, isbn: Int, title: String, genre: String, price: Double? = nil, rating: Double? = nil, publishDate: Date? = nil, ratingCount: Int? = nil, description: String? = nil, isPaperVersionAvaliable: Bool? = nil, pageCount: Int? = nil, isAudibleAvaliable: Bool? = nil, backCoverText: String? = nil, coverImageUrl: String? = nil, authorName: String? = nil, bookData: Data? = nil) {
         self.id = id
         self.isbn = isbn
         self.title = title
         self.genre = genre
+        self.price = price
+        self.rating = rating
+        self.publishDate = publishDate
+        self.ratingCount = ratingCount
+        self.description = description
+        self.isPaperVersionAvaliable = isPaperVersionAvaliable
+        self.pageCount = pageCount
+        self.isAudibleAvaliable = isAudibleAvaliable
+        self.backCoverText = backCoverText
+        self.coverImageUrl = coverImageUrl
+        self.authorName = authorName
+        self.bookData = bookData
+    }
+
+    func asInformation() -> Information {
+        return Information(
+            title: title,
+            genre: genre,
+            description: description,
+            price: price,
+            rating: rating,
+            publishDate: publishDate,
+            ratingCount: ratingCount,
+            isPaperVersionAvaliable: isPaperVersionAvaliable,
+            pageCount: pageCount,
+            isAudibleAvaliable: isAudibleAvaliable,
+            backCoverText: backCoverText,
+            coverImageUrl: coverImageUrl,
+            authorName: authorName
+        )
+    }
+}
+
+extension Book {
+    struct Information: Content {
+        let title: String?
+        let genre: String?
+        let description: String?
+        let price: Double?
+        let rating: Double?
+        let publishDate: Date?
+        let ratingCount: Int?
+        let isPaperVersionAvaliable: Bool?
+        let pageCount: Int?
+        let isAudibleAvaliable: Bool?
+        let backCoverText: String?
+        let coverImageUrl: String?
+        let authorName: String?
+    }
+
+    struct SearchBookQuery: Content {
+        let title: String?
+        let genre: String?
+        let description: String?
+        let authorName: String?
     }
 }
